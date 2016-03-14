f3f52030ab0afb1bfd435a3ba5ea50f9cc5fcf0e
torsinclair <tor@romanesco.se>
2016-03-14 14:39:01 -0700

    Sanitize inputs.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -42,3 +42,42 @@ $.get('https://api.parse.com/1/classes/messages', function(response, status) {
   });
 
 });
+
+// var sanitizeMessage = function(message) {
+//   if (!message) return message;
+//   var retMessage = '';
+//   // var escapeCharacters = ['\&', '\<', '>', '\"', '\'', '`', '!', '@', '$', '%', 
+//   //   '(', ')', '=', '+', '{', '}', '[', ']'];
+//   var escapeCharacters = {
+//     '&': '&#38;',
+//     '<': '&#60;',
+//     '>': '&#62;',
+//     '"': '&#34;',
+//     '\'': '&#39;',
+//     '`': '&#96;',
+//     '!': '&#33;',
+//     '@': '&#64;',
+//     '$': '&#36;',
+//     '%': '&#37;',
+//     '(': '&#40;',
+//     ')': '&#41;',
+//     '=': '&#61;',
+//     '+': '&#43;',
+//     '{': '&#123;',
+//     '}': '&#125;',
+//     '[': '&#91;',
+//     ']': '&#93;'
+//   };
+
+//   for (var i = 0; i < message.length; i++) {
+//     var currentChar = message.charAt(i);
+//     var escapeSequence = escapeCharacters[currentChar]
+//     if (escapeSequence !== undefined) {
+//       retMessage += escapeSequence;
+//     } else {
+//       retMessage += currentChar;
+//     }
+//   }
+
+//   return retMessage;
+// };
