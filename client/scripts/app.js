b4115f5b598d2a7de79581674a207038f8badeff
torsinclair <tor@romanesco.se>
2016-03-14 15:25:02 -0700

    Send messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -57,3 +57,44 @@ setInterval(function() {
   });
 }, 1000);
 
+$(document).ready(function() {
+  $('#new-message').submit(function(e) {
+    e.preventDefault();
+
+    var userName = $('#username').val();
+    var text = $('#text').val();
+
+    var message = {
+      username: userName,
+      text: text,
+      roomname: 'hr40'
+    };
+
+    $.post('https://api.parse.com/1/classes/messages', JSON.stringify(message), function() {
+      console.log('Send succeeded');
+    });
+
+    // $.ajax({
+    //   // This is the url you should use to communicate with the parse API server.
+    //   url: 'https://api.parse.com/1/classes/messages',
+    //   type: 'POST',
+    //   data: JSON.stringify(message),
+    //   contentType: 'application/json',
+    //   success: function (data) {
+    //     console.log('chatterbox: Message sent');
+    //   },
+    //   error: function (data) {
+    //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    //     console.error('chatterbox: Failed to send message', data);
+    //   }
+    // });
+    
+  });
+});
+
+
+// var message = {
+//   username: 'tor',
+//   text: 'testing',
+//   roomname: '4chan'
+// };
