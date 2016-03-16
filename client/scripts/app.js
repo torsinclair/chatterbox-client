37e87f1209526eddc0350b43058ff9085c88cca5
torsinclair <tor@romanesco.se>
2016-03-15 19:15:06 -0700

    Add profile picture

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -89,6 +89,22 @@ $(document).ready(function() {
     $(this).toggleClass('like');
   });
 
+  var readURL = function(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('.profile img').attr('src', e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
+
+  $('#image-input').change(function() {
+    readURL(this);
+  });
+
 });
 
 var addRoom = function(message) {
